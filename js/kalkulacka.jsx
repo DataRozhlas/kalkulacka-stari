26b596dfaa0ad5fbc51ebca1ce5628156268a02d
Petr Kočí <petr.koci@samizdat.cz>
2020-11-23 23:25:09 +0100

    styly

--- js/kalkulacka.jsx
+++ js/kalkulacka.jsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 
 const otazky = [
   {
-    otazka: "Obáváte se, že se ve stáří budete hůř pohybovat a nedokážete si sami dojít, kam potřebujete?",
+    otazka:
+      "Obáváte se, že se ve stáří budete hůř pohybovat a nedokážete si sami dojít, kam potřebujete?",
     odpovedi: [
       "již to zažívám",
       "určitě ano",
@@ -12,113 +13,112 @@ const otazky = [
       "vůbec ne",
       "vůbec na to nemyslím",
     ],
-    body: [1, 1, 1, 1, 1, 1,],
+    body: [1, 1, 1, 1, 1, 1],
   },
   {
     otazka: "Obáváte se, že budete ve stáří osamělí?",
     odpovedi: [
-        "již to zažívám",
-        "určitě ano",
-        "spíše ano",
-        "spíše ne",
-        "vůbec ne",
-        "vůbec na to nemyslím",
-      ],
-    body: [1, 1, 1, 1, 1, 1,],
+      "již to zažívám",
+      "určitě ano",
+      "spíše ano",
+      "spíše ne",
+      "vůbec ne",
+      "vůbec na to nemyslím",
+    ],
+    body: [1, 1, 1, 1, 1, 1],
   },
   {
-    otazka: "Obáváte se, že budete ve stáří vystaveni šikaně nebo domácímu násilí?",
+    otazka:
+      "Obáváte se, že budete ve stáří vystaveni šikaně nebo domácímu násilí?",
     odpovedi: [
-        "již to zažívám",
-        "určitě ano",
-        "spíše ano",
-        "spíše ne",
-        "vůbec ne",
-        "vůbec na to nemyslím",
-      ],
-    body: [1, 1, 1, 1, 1, 1,],
+      "již to zažívám",
+      "určitě ano",
+      "spíše ano",
+      "spíše ne",
+      "vůbec ne",
+      "vůbec na to nemyslím",
+    ],
+    body: [1, 1, 1, 1, 1, 1],
   },
   {
     otazka: "Obáváte se, že ve stáří budete chudí?",
     odpovedi: [
-        "již to zažívám",
-        "určitě ano",
-        "spíše ano",
-        "spíše ne",
-        "vůbec ne",
-        "vůbec na to nemyslím",
-      ],
-    body: [1, 1, 1, 1, 1, 1,],
+      "již to zažívám",
+      "určitě ano",
+      "spíše ano",
+      "spíše ne",
+      "vůbec ne",
+      "vůbec na to nemyslím",
+    ],
+    body: [1, 1, 1, 1, 1, 1],
   },
   {
-    otazka: "Plánujete využívat v důchodovém věku pro financování svých životních potřeb penzijní připojištění?",
+    otazka:
+      "Plánujete využívat v důchodovém věku pro financování svých životních potřeb penzijní připojištění?",
     odpovedi: [
-        "ano, jako hlavní zdroj",
-        "ano, jako důležitý zdroj",
-        "ano, jako okrajový zdroj",
-        "ne",
-      ],
-    body: [1, 1, 1, 1,],
+      "ano, jako hlavní zdroj",
+      "ano, jako důležitý zdroj",
+      "ano, jako okrajový zdroj",
+      "ne",
+    ],
+    body: [1, 1, 1, 1],
   },
   {
-    otazka: "Plánujete využívat v důchodovém věku pro financování svých životních potřeb zaměstnání či podnikání?",
+    otazka:
+      "Plánujete využívat v důchodovém věku pro financování svých životních potřeb zaměstnání či podnikání?",
     odpovedi: [
-        "ano, jako hlavní zdroj",
-        "ano, jako důležitý zdroj",
-        "ano, jako okrajový zdroj",
-        "ne",
-      ],
-    body: [1, 1, 1, 1,],
+      "ano, jako hlavní zdroj",
+      "ano, jako důležitý zdroj",
+      "ano, jako okrajový zdroj",
+      "ne",
+    ],
+    body: [1, 1, 1, 1],
   },
   {
     otazka: "Cvičíte, věnujete se pravidelně nějakému sportu?",
-    odpovedi: [
-        "ano",
-        "ne",
-    ],
-    body: [1, 1,],
+    odpovedi: ["ano", "ne"],
+    body: [1, 1],
   },
   {
     otazka: "Zajímáte se o prevenci či očkování?",
-    odpovedi: [
-        "ano",
-        "ne",
-    ],
-    body: [1, 1,],
+    odpovedi: ["ano", "ne"],
+    body: [1, 1],
   },
   {
-    otazka: "Jak často pomáháte sousedům či známým? Například a zahradě, při opravách nemovitostí, s nákupy nebo jako doprovod při různých pochůzkách...",
+    otazka:
+      "Jak často pomáháte sousedům či známým? Například a zahradě, při opravách nemovitostí, s nákupy nebo jako doprovod při různých pochůzkách...",
     odpovedi: [
-        "denně",
-        "několikrát týdně",
-        "několikrát za měsíc",
-        "několikrát za rok nebo méně často",
-        "nikdy",
+      "denně",
+      "několikrát týdně",
+      "několikrát za měsíc",
+      "několikrát za rok nebo méně často",
+      "nikdy",
     ],
-    body: [1, 1, 1, 1, 1,],
+    body: [1, 1, 1, 1, 1],
   },
   {
-    otazka: "Jak často se účastníte aktivit v zájmových organizacích, například v církvi, v klubu důchodců nebo v politickém hnutí?",
+    otazka:
+      "Jak často se účastníte aktivit v zájmových organizacích, například v církvi, v klubu důchodců nebo v politickém hnutí?",
     odpovedi: [
-        "denně",
-        "několikrát týdně",
-        "několikrát za měsíc",
-        "několikrát za rok nebo méně často",
-        "nikdy",
+      "denně",
+      "několikrát týdně",
+      "několikrát za měsíc",
+      "několikrát za rok nebo méně často",
+      "nikdy",
     ],
-    body: [1, 1, 1, 1, 1,],
+    body: [1, 1, 1, 1, 1],
   },
   {
     otazka: "Kolik je vám let?",
     odpovedi: [
-        "19 - 29 let",
-        "30 - 45 let",
-        "46 - 59 let",
-        "60 - 69 let",
-        "70 a více",
+      "19 - 29 let",
+      "30 - 45 let",
+      "46 - 59 let",
+      "60 - 69 let",
+      "70 a více",
     ],
-    body: [1, 1, 1, 1, 1,],
-  }
+    body: [1, 1, 1, 1, 1],
+  },
 ];
 
 const App = () => {
@@ -127,28 +127,33 @@ const App = () => {
 
   if (otazka < otazky.length) {
     return (
-      <div>
-        {otazky[otazka].otazka}
-        {otazky[otazka].odpovedi.map((o, i) => {
-          return (
-            <button
-              key={i}
-              onClick={() => {
-                const zmenenyOdpovedi = [...odpovedi];
-                zmenenyOdpovedi[otazka] = i;
-                setOdpovedi(zmenenyOdpovedi);
-                const dalsiOtazka = otazka + 1;
-                setOtazka(dalsiOtazka);
-              }}
-            >
-              {o}
-            </button>
-          );
-        })}
+      <div className="quiz-container">
+        <div className="quiz-body">
+          <div className="question-text">{otazky[otazka].otazka}</div>
+          <div className="button-options">
+            {otazky[otazka].odpovedi.map((o, i) => {
+              return (
+                <button
+                  className="quiz-button quiz-button-option"
+                  key={i}
+                  onClick={() => {
+                    const zmenenyOdpovedi = [...odpovedi];
+                    zmenenyOdpovedi[otazka] = i;
+                    setOdpovedi(zmenenyOdpovedi);
+                    const dalsiOtazka = otazka + 1;
+                    setOtazka(dalsiOtazka);
+                  }}
+                >
+                  {o}
+                </button>
+              );
+            })}
+          </div>
+        </div>
       </div>
     );
   } else {
-    return <div>Vyhodnocení, woe</div>;
+    return <div className="quiz-container">Vyhodnocení, woe</div>;
   }
 };
 
